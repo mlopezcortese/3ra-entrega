0d6455881201e9bb547e71c1551222050b38c8c8
Matias Lopez Cortese <matais.lopez.89@hotmail.com>
2023-02-06 22:25:11 -0300

    main

---
+++ stock.js
@@ -0,0 +1,14 @@
+let stockProductos = [
+    {id: 1, nombre: "Paquete Impulso", tipo: "Paquete", cantidad: 1, desc: "Caja de ahorro, tarjeta de credito", precio: 1200, img: './img/impulso.png'},
+    {id: 2, nombre: "Paquete Crecimiento", tipo: "Paquete", cantidad: 1, desc: "Caja de ahorro, tarjeta de credito", precio: 2100, img: './img/crecimiento.png'},
+    {id: 3, nombre: "Paquete Evolucion", tipo: "Paquete", cantidad: 1, desc: "Caja de ahorro, tarjeta de credito", precio: 3200, img: './img/evolucion.png'},
+    {id: 4, nombre: "Paquete Logros", tipo: "Paquete", cantidad: 1, desc: "Caja de ahorro, tarjeta de credito", precio: 4400, img: './img/logros.png'},
+    {id: 5, nombre: "Paquete Impulso", tipo: "Paquete", cantidad: 1, desc: "Caja de ahorro, tarjeta de credito", precio: 1200, img: './img/impulso.png'},
+    {id: 6, nombre: "Paquete Crecimiento", tipo: "Paquete", cantidad: 1, desc: "Caja de ahorro, tarjeta de credito", precio: 2100, img: './img/crecimiento.png'},
+    {id: 7, nombre: "Paquete Evolucion", tipo: "Paquete", cantidad: 1, desc: "Caja de ahorro, tarjeta de credito", precio: 3200, img: './img/evolucion.png'},
+    {id: 8, nombre: "Paquete Logros", tipo: "Paquete", cantidad: 1, desc: "Caja de ahorro, tarjeta de credito", precio: 4400, img: './img/logros.png'},{id: 1, nombre: "Paquete Impulso", tipo: "Paquete", cantidad: 1, desc: "Caja de ahorro, tarjeta de credito", precio: 1200, img: './img/impulso.png'},
+    {id: 9, nombre: "Paquete Crecimiento", tipo: "Paquete", cantidad: 1, desc: "Caja de ahorro, tarjeta de credito", precio: 2100, img: './img/crecimiento.png'},
+    {id: 10, nombre: "Paquete Evolucion", tipo: "Paquete", cantidad: 1, desc: "Caja de ahorro, tarjeta de credito", precio: 3200, img: './img/evolucion.png'},
+    {id: 11, nombre: "Paquete Logros", tipo: "Paquete", cantidad: 1, desc: "Caja de ahorro, tarjeta de credito", precio: 4400, img: './img/logros.png'},
+]
+
